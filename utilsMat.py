009a3c79da4357c5a75ef0af6672f9aa2acff620
Matisse Van Schalkwijk <matisse.vanschalkwijk@epfl.ch>
2024-11-11 19:08:45 +0100

    bon...

---
+++ utilsMat.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+
+
+def contains_category(x, category):
+    if x is None : 
+        return False
+    for elt in x :
+        if elt is None :
+            continue
+        if category in elt :
+            return True
+    return False
+
+def get_category(article, categories):
+    try:
+        return categories[categories["article"] == article]["category"].values[0]
+    except:
+        return None
+
+
